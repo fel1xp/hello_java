7e9276488e6881b4b824d9dc8ef2bd1376b34805
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-09-27 16:09:08 +0100

    working with variables

--- java/section1/HelloWorld/src/Hello.java
+++ java/section1/HelloWorld/src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFirstNumber = 5 + 2;
+        System.out.println(myFirstNumber);
     }
 }
